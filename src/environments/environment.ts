6b2d4ae83c7bf5804587fa949bd1b7e69b198595
Niver Copa <47153990+vlader122@users.noreply.github.com>
2024-09-30 08:12:41 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   app: 'SISAUTO',
-  rutaBackend: 'http://vlader122-001-site1.atempurl.com',
+  rutaBackend: 'https://vlader122-001-site1.atempurl.com',
   token: 'token'
 };
 
